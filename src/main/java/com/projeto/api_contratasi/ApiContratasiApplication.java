bf3d193539360913634f67269ee1bfd5d099be14
Alisson Neves <alissondasneves@hotmail.com>
2025-10-09 21:39:20 -0300

    build: criação do projeto base via Spring Initializr

---
+++ src/main/java/com/projeto/api_contratasi/ApiContratasiApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.api_contratasi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiContratasiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiContratasiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto/api_contratasi/ApiContratasiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.api_contratasi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiContratasiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
